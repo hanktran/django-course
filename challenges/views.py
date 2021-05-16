aa526d22b4844bca3869072aba474654bd5349d1
Han Tran <imhantran@gmail.com>
2021-05-16 10:28:29 +0700

    add templates, dynamic templates, style files

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
@@ -20,16 +20,11 @@ monthly_challenges = {
 }
 
 def index(request):
-  list_items = ""
   months = list(monthly_challenges.keys())
 
-  for month in months:
-    capitalized_month = month.capitalize()
-    month_path = reverse("month-challenge", args=[month])
-    list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-  response_data = f"<ul>{list_items}</ul>"
-  return HttpResponse(response_data)
+  return render(request, "challenges/index.html", {
+    "months": months
+  })
 
 def monthly_challenge_number(request, month):
   months = list(monthly_challenges.keys())
@@ -44,7 +39,9 @@ def monthly_challenge_number(request, month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
-    response_data = f"<h1>{challenge_text}</h1>"
-    return HttpResponse(response_data)
+    return render(request ,"challenges/challenge.html", {
+      "text": challenge_text,
+      "month_name": month
+    })
   except:
-    return HttpResponseNotFound("This month is not supported!")
+    raise Http404()
